5c011522c1c8072051c295e4d0f741f386e57e22
addy2354 <addy2354@gmail.com>
2024-02-01 04:28:11 +0530

    Update 3

--- src/context/shows/showsState.js
+++ src/context/shows/showsState.js
@@ -1,71 +1,93 @@
-import { useReducer } from 'react';
-import axios from 'axios';
-import ShowsContext from './showsContext';
-import ShowsReducer from './showsReducer';
+import { useReducer, useEffect } from "react";
+import axios from "axios";
+import ShowsContext from "./showsContext";
+import ShowsReducer from "./showsReducer";
 import {
-    SEARCH_SHOWS,
-    SET_LOADING,
-    SET_SINGLE_SHOW,
-    CLEAR_SINGLE_SHOW
-} from '../types';
+  SEARCH_SHOWS,
+  SET_LOADING,
+  SET_SINGLE_SHOW,
+  CLEAR_SINGLE_SHOW,
+} from "../types";
 
 const ShowsState = (props) => {
-    const initialState = {
-        shows: [],
-        singleShow: {},
-        loading: false
-    }
+  const initialState = {
+    shows: [],
+    singleShow: {},
+    loading: true,
+  };
 
-    const [state, dispatch] = useReducer(ShowsReducer, initialState)
+  const [state, dispatch] = useReducer(ShowsReducer, initialState);
 
-    const searchShows = async (searchTerm) => {
-        dispatch({type: SET_LOADING});
+  useEffect(() => {
+    const fetchAllShows = async () => {
+      dispatch({ type: SET_LOADING });
 
-        const { data } = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-
-        console.log(data);
+      try {
+        const { data } = await axios.get("https://api.tvmaze.com/search/shows?q=all");
 
         dispatch({
-            type: SEARCH_SHOWS,
-            payload: data
+          type: SEARCH_SHOWS,
+          payload: data,
         });
-    }
-
-    const getSingleShow = async (id) => {
-        dispatch({
-            type: SET_LOADING,
-        });
-
-        const { data } = await axios.get(`https://api.tvmaze.com/shows/${id}`);
-
-        console.log(data);
-
-        dispatch({
-            type: SET_SINGLE_SHOW,
-            payload: data
-        })
+      } catch (error) {
+        console.error("Error fetching all shows:", error);
+        // You may want to handle the error and update the state accordingly
+      }
     };
 
-    const clearSingleShow = () => {
-        dispatch({
-            type: CLEAR_SINGLE_SHOW
-        })
-    }
+    fetchAllShows();
+  }, []);
 
-    return (
-        <ShowsContext.Provider
-            value={{
-                shows: state.shows,
-                singleShow: state.singleShow,
-                loading: state.loading,
-                searchShows,
-                getSingleShow,
-                clearSingleShow
-            }}
-        >
-            {props.children}
-        </ShowsContext.Provider>
-    )
-}
+  const searchShows = async (searchTerm) => {
+    dispatch({ type: SET_LOADING });
 
-export default ShowsState
+    const { data } = await axios.get(
+      `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+    );
+
+    console.log(data);
+
+    dispatch({
+      type: SEARCH_SHOWS,
+      payload: data,
+    });
+  };
+
+  const getSingleShow = async (id) => {
+    dispatch({
+      type: SET_LOADING,
+    });
+
+    const { data } = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+
+    console.log(data);
+
+    dispatch({
+      type: SET_SINGLE_SHOW,
+      payload: data,
+    });
+  };
+
+  const clearSingleShow = () => {
+    dispatch({
+      type: CLEAR_SINGLE_SHOW,
+    });
+  };
+
+  return (
+    <ShowsContext.Provider
+      value={{
+        shows: state.shows,
+        singleShow: state.singleShow,
+        loading: state.loading,
+        searchShows,
+        getSingleShow,
+        clearSingleShow,
+      }}
+    >
+      {props.children}
+    </ShowsContext.Provider>
+  );
+};
+
+export default ShowsState;
